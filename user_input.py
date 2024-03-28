2530807082845a5172413449f861216085a65c45
Shane Tech <128010804+shaneeiesen@users.noreply.github.com>
2024-03-28 20:08:20 +0200

    InputFunction_Test

---
+++ user_input.py
@@ -0,0 +1,7 @@
+#: Python_Code_inputFunction_test
+
+name = input("Please enter your name below: ")
+age = input("Please enter your age below: ")
+location = input("Please enter your location below: ")
+
+print("Hello " + name + ", you are " + age + " years old and you live at " + location + ".")
